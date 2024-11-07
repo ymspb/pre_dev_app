9ee969ba89b7ed0a862787a223c210b52a7aaacf
ymspb <masayayamamoto99@gmail.com>
2024-11-07 10:36:30 +0900

    [Finish] 3-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get "lists/new"
+  get "lists/index"
+  get "lists/show"
+  get "lists/edit"
+  get "/top" => "homes#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
